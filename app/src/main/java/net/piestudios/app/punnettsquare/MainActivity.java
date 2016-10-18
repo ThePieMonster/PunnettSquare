5a60cbe2057a69bc1e5f1abfff40a18aeefb4f39
ThePieMonster <zacbraasch@gmail.com>
2016-10-17 23:55:18 -0400

    Added menu

--- app/src/main/java/net/piestudios/app/punnettsquare/MainActivity.java
+++ app/src/main/java/net/piestudios/app/punnettsquare/MainActivity.java
@@ -1,9 +1,12 @@
 package net.piestudios.app.punnettsquare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
@@ -15,6 +18,24 @@ import android.text.InputFilter;
 import android.text.Spanned;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Show menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // Interact with menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.settings:
+                //startActivity(new Intent(this, settings.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     // Number of traits box and variable
     EditText numberoftraitslistner;
